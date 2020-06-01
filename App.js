3338002e750c58564eaddc408e6875e6aa3e3d3a
mkef01 <mkef01@gmail.com>
2020-06-01 16:03:52 -0600

    hola a todos

--- App.js
+++ App.js
@@ -17,3 +17,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// Client ID kHqI9yBZaGkB8J_UAVjwMQ
+//API Key wVQo_U121P-nCunVZKVTaqzERnX3_JsqIoxgiu9-Q5d3Gh32Uw7QWbDKvrEZD7T2TzPGYzNiKzkPV0j_wlWZ497uX0k4IWxxnGovHWECCViZxItYOd6I5y-Jw3PVXnYx
